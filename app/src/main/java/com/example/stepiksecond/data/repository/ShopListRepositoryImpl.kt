6892ae2cfca751a66c2497469dcfc90f034d3300
timur200362 <kh-g-2016@mail.ru>
2024-03-20 04:23:45 +0300

    Lesson3: data-слой

---
+++ app/src/main/java/com/example/stepiksecond/data/repository/ShopListRepositoryImpl.kt
@@ -0,0 +1,37 @@
+package com.example.stepiksecond.data.repository
+
+import com.example.stepiksecond.domain.entity.ShopItem
+import com.example.stepiksecond.domain.repository.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl: ShopListRepository { //object чтобы был синглтон
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID){
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        shopList.add(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+         return shopList.toMutableList()
+    }
+}
--- app/src/main/java/com/example/stepiksecond/domain/entity/ShopItem.kt
+++ app/src/main/java/com/example/stepiksecond/domain/entity/ShopItem.kt
@@ -1,8 +1,12 @@
 package com.example.stepiksecond.domain.entity
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
